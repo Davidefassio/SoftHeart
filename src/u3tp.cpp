a0ef5788014e51dfe43c54bf8ec1d543641bacdd
Davide Fassio <davidefassio7@gmail.com>
2022-12-30 13:12:48 +0100

    Start of the new branch

---
+++ src/u3tp.cpp
@@ -0,0 +1,20 @@
+#include "u3tp.hpp"
+
+#include <iostream>
+#include <string>
+
+namespace
+{
+    
+}
+
+// Process input, call action, print output
+void sh::U3tp::loop()
+{
+    std::string input, output;
+
+    while(true)
+    {
+        std::cin >> input;
+    }
+}
---
+++ src/u3tp.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace sh
+{
+    class U3tp
+    {
+    public:
+        U3tp() = default;
+
+        void loop();
+    };
+}
