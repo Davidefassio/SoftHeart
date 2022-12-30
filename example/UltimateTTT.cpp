6ce3fc706c2cee51e0434bcfc6675e4154d3d5ec
Davide Fassio <davidefassio7@gmail.com>
2022-12-30 13:12:48 +0100

    Start of the new branch

--- example/UltimateTTT.cpp
+++
@@ -1,9 +0,0 @@
-#include "Game.hpp"
-
-int main(int argc, char* argv[]) 
-{	
-	GameApp game;
-	game.run();
-
-	return 0;
-}
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "u3tp.hpp"
+
+int main(int argc, char* argv[])
+{
+    sh::U3tp iface;
+    iface.loop();
+    
+    return 0;
+}
